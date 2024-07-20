93dbb1e5f068730f092b70575f612356edcead21
Karol Topchiev <karlostopchiev@gmail.com>
2024-07-20 18:30:49 +0300

    added Router to index.js and App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import PostPage from "./PostPage";
 import NewPost from "./NewPost";
 import About from "./About";
 import Missing from "./Missing";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Nav />
+      <Home />
+      <Footer />
+      <PostPage />
+      <NewPost />
+      <About />
+      <Missing />
     </div>
   );
 }
