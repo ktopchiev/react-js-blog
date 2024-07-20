1e4d7700fd049d8f37480f978d7cda62ff8d529e
Karol Topchiev <karlostopchiev@gmail.com>
2024-07-20 18:18:06 +0300

    Created pages components and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Home from "./Home";
+import Footer from "./Footer";
+import PostPage from "./PostPage";
+import NewPost from "./NewPost";
+import About from "./About";
+import Missing from "./Missing";
+
 function App() {
   return (
     <div className="App">
@@ -5,5 +14,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
